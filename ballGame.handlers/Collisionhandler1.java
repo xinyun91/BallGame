e75c379948f99c9ccd1d29c046df375c852ecc6c
xinyun91 <xinyun91@users.noreply.github.com>
2015-03-24 11:33:44 -0400

    Create Collisionhandler1.java

---
+++ ballGame.handlers/Collisionhandler1.java
@@ -0,0 +1,19 @@
+package lab6.handlers;
+
+import engine.Entity;
+import engine.collision.CollisionListener;
+
+public class Collisionhandler1 implements CollisionListener {
+    
+    public Collisionhandler1() {
+
+    }
+
+    @Override
+    public void collisionOccurred(Entity e) {
+        // TODO Auto-generated method stubs    
+        //if north or south wall hits with the entity e, e negates its y velocity
+        e.setYVelocity(-e.getYVelocity());
+    
+    }
+}
