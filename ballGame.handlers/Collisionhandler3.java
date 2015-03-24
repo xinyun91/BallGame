8a54b354101525f29e5c9ae292cfca5a14ca5828
xinyun91 <xinyun91@users.noreply.github.com>
2015-03-24 11:34:36 -0400

    Create Collisionhandler3.java

---
+++ ballGame.handlers/Collisionhandler3.java
@@ -0,0 +1,24 @@
+package lab6.handlers;
+
+import engine.Entity;
+import engine.collision.CollisionListener;
+
+public class Collisionhandler3 implements CollisionListener {
+
+    public Collisionhandler3() {
+        
+    }
+
+    @Override
+    public void collisionOccurred(Entity e) {
+        // TODO Auto-generated method stub
+        if (e.getXCoordinate() > 400){
+        System.out.println("Blue Wins!");
+        }
+        else {
+            System.out.println("Green Wins!");
+        }
+        System.exit(0);
+    }
+
+}
