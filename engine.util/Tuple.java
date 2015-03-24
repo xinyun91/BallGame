fea413cf3d20dbd38098de7299188b488819d7a1
xinyun91 <xinyun91@users.noreply.github.com>
2015-03-24 11:59:48 -0400

    Create Tuple.java

---
+++ engine.util/Tuple.java
@@ -0,0 +1,12 @@
+package engine.util;
+
+public class Tuple<E,K> {
+    public E _e;
+    public K _k;
+    
+    public Tuple(E e, K k) {
+        // TODO Auto-generated constructor stub
+        _e = e;
+        _k = k;
+    }
+}
