2d39ed9d467acb0c30ad34b2cfbea718c923abf4
xinyun91 <xinyun91@users.noreply.github.com>
2015-03-24 11:49:55 -0400

    Create InvalidImageFileException.java

---
+++ engine.errors/InvalidImageFileException.java
@@ -0,0 +1,22 @@
+package engine.errors;
+
+import java.io.IOException;
+
+public class InvalidImageFileException extends IOException{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public InvalidImageFileException() {
+        // TODO Auto-generated constructor stub
+        super();
+    }
+    
+    public InvalidImageFileException(String message) {
+        // TODO Auto-generated constructor stub
+        super(message);
+    }
+
+}
